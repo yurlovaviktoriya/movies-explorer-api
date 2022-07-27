555756a32f2a013265ce3b8e0b59075d1eaeaa07
Victoriya Yurlova <yurlovaviktoriya@yandex.ru>
2022-07-27 20:26:13 +0700

    feat: add getCurrentUserInfo and updateCurrentUserInfo controllers

--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,21 @@
-const getCurrentUserInfo = () => {
-  console.log('getCurrentUserInfo');
+const User = require('../models/user');
+
+const getCurrentUserInfo = (req, res, next) => {
+  User.getById(req.user_id)
+    .then((data) => {
+      res.send(data);
+    }).catch(next);
 };
 
-const updateCurrentUserInfo = () => {
-  console.log('updateCurrentUserInfo');
+const updateCurrentUserInfo = (req, res, next) => {
+  const { name, email } = req.body;
+  User.getByIdAndUpdate(
+    req.user._id,
+    { name, email },
+    { new: true, runValidators: true }
+  ).then((data) => {
+    res.send(data);
+  }).catch(next);
 };
 
 module.exports = {
