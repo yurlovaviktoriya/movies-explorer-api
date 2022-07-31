15efc2d1d5c50e3ad2ecf95333abff5620c4fd23
Victoriya Yurlova <yurlovaviktoriya@yandex.ru>
2022-07-31 16:12:58 +0700

    feat: add auth validators

--- middlewares/validation/users.js
+++ middlewares/validation/users.js
@@ -2,6 +2,21 @@ const { celebrate, Joi } = require('celebrate');
 
 const { regexLink } = require('./regex');
 
+const signupValidator = celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+    name: Joi.string().min(2).max(30)
+  })
+});
+
+const signinValidator = celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8)
+  })
+});
+
 const updateUserInfoValidator = celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
@@ -10,5 +25,7 @@ const updateUserInfoValidator = celebrate({
 });
 
 module.exports = {
+  signupValidator,
+  signinValidator,
   updateUserInfoValidator
 };
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,11 @@ const router = require('express').Router();
 
 const usersRouter = require('./users');
 const moviesRouter = require('./movies');
+const { signupValidator, signinValidator } = require('../middlewares/validation/users');
+const { register, login } = require('../controllers/users');
+
+router.use('/signup', signupValidator, register);
+router.use('/signin', signinValidator, login);
 
 router.use('/users', usersRouter);
 router.use('/movies', moviesRouter);
