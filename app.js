40f9258bfe64f75a6eb6c924965481e71053b97a
Victoriya Yurlova <yurlovaviktoriya@yandex.ru>
2022-07-31 16:48:03 +0700

    fix: add dotenv, body and cookie parsers

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+require('dotenv').config();
 
 const { router } = require('./routes/index');
 
 const { PORT = 3000, DB_PATH, NODE_ENV } = process.env;
 
 const app = express();
-
 mongoose.connect(NODE_ENV !== 'production' ? 'mongodb://localhost:27017/moviesdb' : DB_PATH);
+
+app.use(bodyParser.json());
+app.use(cookieParser());
 
 app.use(router);
 
