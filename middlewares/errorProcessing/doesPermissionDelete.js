3c415b9ca4b59ba5e4734e31f6dd416a5e85ef5a
Victoriya Yurlova <yurlovaviktoriya@yandex.ru>
2022-08-01 16:14:31 +0700

    fix: change from card to movie

--- middlewares/errorProcessing/doesPermissionDelete.js
+++ middlewares/errorProcessing/doesPermissionDelete.js
@@ -3,7 +3,7 @@ const ForbiddenError = require('../../errorClasses/ForbiddenError');
 module.exports.doesPermissionDelete = (req, res, next) => {
   if (!(String(req.movie.owner) === String(req.user._id))) {
     throw new ForbiddenError('Нельзя удалять фильм, сохранённый другим пользователем. '
-      + `Автор записи ${req.card.owner} и текущий пользователь ${req.user._id} не совпадают`);
+      + `Автор записи ${req.movie.owner} и текущий пользователь ${req.user._id} не совпадают`);
   }
   next();
 };
