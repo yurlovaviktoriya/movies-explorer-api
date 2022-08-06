5fc62eb66fcf1b1af5bd72f1454519036a0f53c3
Victoriya Yurlova <yurlovaviktoriya@yandex.ru>
2022-08-06 11:59:18 +0700

    fix: delete limit password length

--- middlewares/validation/users.js
+++ middlewares/validation/users.js
@@ -3,7 +3,7 @@ const { celebrate, Joi } = require('celebrate');
 const signupValidator = celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
-    password: Joi.string().required().min(8),
+    password: Joi.string().required(),
     name: Joi.string().required().min(2).max(30),
   }),
 });
@@ -11,7 +11,7 @@ const signupValidator = celebrate({
 const signinValidator = celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
-    password: Joi.string().required().min(8),
+    password: Joi.string().required(),
   }),
 });
 
