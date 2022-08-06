e0693117f57eb4d437ae58ec8c8b218a055a6af1
Victoriya Yurlova <yurlovaviktoriya@yandex.ru>
2022-08-06 12:03:46 +0700

    fix: add required field to updateUsrInfo

--- middlewares/validation/users.js
+++ middlewares/validation/users.js
@@ -17,8 +17,8 @@ const signinValidator = celebrate({
 
 const updateUserInfoValidator = celebrate({
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
-    email: Joi.string().email(),
+    name: Joi.string().required().min(2).max(30),
+    email: Joi.string().required().email(),
   }),
 });
 
